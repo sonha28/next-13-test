76e9f60574f24e0b5e1edfb147253b4c3af6eb0f
Hoang Anh Son (NHS-HO) <sonha@pvcombank.com.vn>
2024-06-29 00:06:54 +0700

    itnit

---
+++ src/pages/next-page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NextPage = () => {
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+        >
+            <h1>Next Page</h1>
+        </main>
+    );
+};
+
+export default NextPage;
